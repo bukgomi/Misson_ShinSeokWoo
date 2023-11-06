64bdcd14748b8f4356b6760023ffde4429b9f1a5
BukGomi <bukgomi@naver.com>
2023-11-06 16:10:00 +0900

    2_

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,16 +1,20 @@
 package com.ll;
-
 import java.util.Scanner;
-
 public class App {
     void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
+        while (true) {
+            System.out.print("명령) ");
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+            if (cmd.equals("종료")) {
+                break;
+            }
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
-
-        System.out.printf("입력받은 명령어 : %s\n", cmd);
-
+            else if(cmd.equals("등록")) {
+                System.out.println("명언 : ");
+                System.out.println("작가 : ");
+            }
+        }
     }
 }
